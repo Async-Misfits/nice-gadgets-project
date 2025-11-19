0d6e21e7e6a31c6d7470a0135c405ddd29a1cb89
Daria Serebriakova <[  ]>
2025-11-19 15:19:14 +0200

    Fix iconMap imports

--- src/components/base/icons/iconMap.ts
+++ src/components/base/icons/iconMap.ts
@@ -1,31 +1,33 @@
-import HomeSvg from '../../../../../public/icons/home.svg?react';
+import HomeSvg from '../../../../public/icons/home.svg?react';
 // import SearchSvg from '../../../../../public/icons/search.svg?react';
-import MailSvg from '../../../../../public/icons/mail.svg?react';
-import HeartSvg from '../../../../../public/icons/heart.svg?react';
-import HeartFilledSvg from '../../../../../public/icons/heart-filled.svg?react';
-import MinusSvg from '../../../../../public/icons/minus.svg?react';
-import PlusSvg from '../../../../../public/icons/plus.svg?react';
-import XSvg from '../../../../../public/icons/x.svg?react';
-import ChevronLeftSvg from '../../../../../public/icons/chevron-left.svg?react';
-import ChevronRightSvg from '../../../../../public/icons/chevron-right.svg?react';
-import ChevronDownSvg from '../../../../../public/icons/chevron-down.svg?react';
-import ChevronUpSvg from '../../../../../public/icons/chevron-up.svg?react';
-import MenuSvg from '../../../../../public/icons/menu.svg?react';
+import MailSvg from '../../../../public/icons/mail.svg?react';
+import HeartSvg from '../../../../public/icons/heart.svg?react';
+import HeartFilledSvg from '../../../../public/icons/heart-filled.svg?react';
+import MinusSvg from '../../../../public/icons/minus.svg?react';
+import PlusSvg from '../../../../public/icons/plus.svg?react';
+import XSvg from '../../../../public/icons/x.svg?react';
+import ChevronLeftSvg from '../../../../public/icons/chevron-left.svg?react';
+import ChevronRightSvg from '../../../../public/icons/chevron-right.svg?react';
+import ChevronDownSvg from '../../../../public/icons/chevron-down.svg?react';
+import ChevronUpSvg from '../../../../public/icons/chevron-up.svg?react';
+import MenuSvg from '../../../../public/icons/menu.svg?react';
+import LogoSvg from '../../../../public/icons/Logo.svg?react';
 
 export const iconMap = {
-  home: HomeSvg,
-//   search: SearchSvg,
-  mail: MailSvg,
-  heart: HeartSvg,
+  'home': HomeSvg,
+  //   search: SearchSvg,
+  'mail': MailSvg,
+  'heart': HeartSvg,
   'heart-filled': HeartFilledSvg,
-  minus: MinusSvg,
-  plus: PlusSvg,
-  x: XSvg,
+  'minus': MinusSvg,
+  'plus': PlusSvg,
+  'x': XSvg,
   'chevron-left': ChevronLeftSvg,
   'chevron-right': ChevronRightSvg,
   'chevron-down': ChevronDownSvg,
   'chevron-up': ChevronUpSvg,
-  menu: MenuSvg,
+  'menu': MenuSvg,
+  'logo': LogoSvg,
 };
 
 export type IconName = keyof typeof iconMap;
