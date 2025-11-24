9875984f3f4e66a9ce2209a71592c7a2b1736f9c
Ievgen Petryk <jakeshad123@gmail.com>
2025-11-24 12:53:24 +0200

    fix-product-list

--- src/components/base/ProductsList/ProductsList.tsx
+++ src/components/base/ProductsList/ProductsList.tsx
@@ -2,22 +2,25 @@ import React from 'react';
 import ProductCard from '../ProductCard/ProductCard';
 import { Grid } from '../../layout/Grid';
 import styles from './ProductsList.module.scss';
-
-export interface Product {
-  id: string | number;
-  title: string;
-  priceRegular: number;
-  priceDiscount: number;
-  screen: string;
-  capacity: string;
-  ram: string;
-}
-
+import type { Product } from '../../../types/Product';
 interface ProductsListProps {
   products: Product[];
+
+  toggleFavorite: () => void;
+  addToCart: () => void;
+
+  isAdded?: boolean;
+  isFavorite?: boolean;
+     
 }
 
-export const ProductsList: React.FC<ProductsListProps> = ({ products }) => {
+export const ProductsList: React.FC<ProductsListProps> = ({ 
+  products,
+  toggleFavorite,
+  addToCart,
+  isAdded = true,
+  isFavorite = false,
+ }) => {
   return (
     <Grid>
       {products.map((product) => (
@@ -26,13 +29,14 @@ export const ProductsList: React.FC<ProductsListProps> = ({ products }) => {
           className={styles.cardItem}
         >
           <ProductCard
-            title={product.title}
-            priceRegular={product.priceRegular}
-            priceDiscount={product.priceDiscount}
-            screen={product.screen}
-            capacity={product.capacity}
-            ram={product.ram}
+            product={product}
             isCatalog={true}
+
+            toggleFavorite={toggleFavorite}
+            addToCart={addToCart}
+
+            isAdded={isAdded}
+            isFavorite={isFavorite}
           />
         </div>
       ))}
