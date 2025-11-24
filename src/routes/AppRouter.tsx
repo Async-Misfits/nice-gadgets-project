6777dbdd1679a5b5ea15380087db5c05819647b2
DmytroLymar <dmytrolymar901@gmail.com>
2025-11-24 14:28:16 +0200

    fix: fixed images added to router

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -2,6 +2,7 @@ import { createHashRouter } from 'react-router';
 import App from '../app/App';
 import { HomePage } from '../pages/HomePage/HomePage';
 import { FavoritesPage } from '../pages/FavoritesPage/FavoritesPage';
+import { ItemPage } from '../pages/ItemPage';
 
 export const router = createHashRouter([
   {
@@ -10,8 +11,11 @@ export const router = createHashRouter([
     children: [
       { index: true, element: <HomePage /> },
       { path: 'phones', element: <div>phones</div> },
+      { path: 'phones/:itemId', element: <ItemPage /> },
       { path: 'tablets', element: <div>tablets</div> },
+      { path: 'tablets/:itemId', element: <ItemPage /> },
       { path: 'accessories', element: <div>accessories</div> },
+      { path: 'accessories/:itemId', element: <ItemPage /> },
       { path: 'cart', element: <div>cart</div> },
       { path: 'favorites', element: <FavoritesPage /> },
       { path: '*', element: <div>not found</div> },
