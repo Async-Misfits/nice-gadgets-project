35e83a4a12397a8e8889fd63abc3561c1c2aecfb
DmytroLymar <dmytrolymar901@gmail.com>
2025-11-25 14:52:23 +0200

    feat: add 404 page add product not found page

---
+++ src/pages/Page404/Page404.tsx
@@ -0,0 +1,41 @@
+import { useNavigate } from 'react-router';
+import { Button } from '../../components/base/Button';
+import { Typography } from '../../components/base/Typography';
+
+import styles from './Page404.module.scss';
+
+export const Page404 = () => {
+  const navigate = useNavigate();
+
+  return (
+    <section className={styles.root}>
+      <div className={styles.content}>
+        <Typography
+          as="h1"
+          variant="h1"
+          className={styles.title}
+        >
+          Oops! Page not found
+        </Typography>
+
+        <Typography
+          variant="body"
+          className={styles.description}
+        >
+          The page you’re looking for doesn’t exist or has been moved.
+        </Typography>
+
+        <div className={styles.imageWrapper}>
+          <div className={styles.imageBg}></div>
+          <img
+            src="/gadgets/img/page-not-found.png"
+            alt="404 cat"
+            className={styles.image}
+          />
+        </div>
+
+        <Button onClick={() => navigate('/')}>Go to homepage</Button>
+      </div>
+    </section>
+  );
+};
